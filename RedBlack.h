f1e1af1d59c969f251faf112658e5baaf5335690
eeshashah <73673633+eeshashah@users.noreply.github.com>
2023-04-20 20:11:54 -0400

    Update RedBlack.h

--- RedBlack.h
+++ RedBlack.h
@@ -4,33 +4,32 @@
 using namespace std;
 
 struct Node {
-    bool red;
-    string genre;
+    string color;
+    string author;
+    vector<string> genre;
     string title;
-    string review;
-    int length;
-    string timePeriod;
+    string description;
+    int pages;
     double rating;
     Node *left;
     Node *right;
     Node *parent;
-    Node (string genre, string title, string review, int length, string timePeriod, double rating): red(true), genre(genre),       title(title), review(review), length(length), timePeriod(timePeriod), rating(rating), left(nullptr), right(nullptr), parent(nullptr){};
-  };
+    Node (vector<string> genre, string title, string description, int pages, string author, double rating): color("red"), genre(genre), title(title),
+    author(author), pages(pages), description(description), rating(rating), left(nullptr), right(nullptr), parent(nullptr){};
+};
 
 class RedBlack {
+    Node* insert(vector<string> genre, string title, string description,int pages, string author, double rating);
 public:
-  Node* insert(Node* root, string genre, string title, string review,int length, string timePeriod, double rating);
-  void insertHelper(string genre, string title, string review,int length, string timePeriod, double rating);
-  Node* balance(Node* node);
-  Node* RBTreeGetGrandparent(Node* node);
-  Node* RBTreeGetParent(Node* node);
-  void print();
-  Node* rightRotate();
-  Node* leftRotate();
-  Node* rightLeftRotate();
-  Node* leftRightRotate();
-  void uniqueBookTitle();
-  void BFS();
-  void DFS();
-  Node* root;
+    Node* root;
+    void balance(Node* node);
+    Node* RBTreeGetGrandparent(Node* node);
+    Node* RBTreeGetUncle(Node* node);
+    void print();
+    void rightRotate(Node* node);
+    void leftRotate(Node* node);
+    void uniqueBookTitle(Node* node);
+    void BFS();
+    void DFS();
+    RedBlack():root(){};
 };
