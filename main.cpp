6dd8a842c7845482fc62f4dad0fd15c09068082b
eeshashah <73673633+eeshashah@users.noreply.github.com>
2023-04-22 16:05:30 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,56 +5,30 @@
 #include <iomanip>
 #include <sstream>
 #include <fstream>
+#include <chrono>
+using namespace std::chrono;
 #include "RedBlack.h"
 using namespace std;
 using std::ofstream;
 
 // READ-ar
 int main() {
+   //Reading in Values and Making the Tree
     RedBlack tree;
-    set<string> genres;
-
-    // Title,description,authors,image,previewLink,publisher,publishedDate,infoLink,categories,ratingsCount
-    cout << endl;
-    cout << "       █░█░█  █▀▀  █    █▀▀  █▀█  █▀▄▀█  █▀▀   ▀█▀  █▀█\n"
-            "       ▀▄▀▄▀  ██▄  █▄▄  █▄▄  █▄█  █░▀░█  ██▄   ░█░  █▄█\n"
-            "                                                                                     ██\n"
-            "     ██████╗░███████╗░█████╗░██████╗░░░░░░░░█████╗░██████╗░                ░░░░░░  ██  ██\n"
-            "     ██╔══██╗██╔════╝██╔══██╗██╔══██╗░░░░░░██╔══██╗██╔══██╗    ▄▀▀▀▀▀▀▄    ░░██░░    ██\n"
-            "     ██████╔╝█████╗░░███████║██║░░██║█████╗███████║██████╔╝   █╗░░▐░░░╔█   ░░██░░ ░░░░░░░\n"
-            "     ██╔══██╗██╔══╝░░██╔══██║██║░░██║╚════╝██╔══██║██╔══██╗   █║░░▐▄▄░║█   ░░██░░ ░░███░░\n"
-            "     ██║░░██║███████╗██║░░██║██████╔╝░░░░░░██║░░██║██║░░██║   █╝░░░░░░╚█   ░░██░░ ░░███░░\n"
-            "     ╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═════╝░░░░░░░╚═╝░░╚═╝╚═╝░░╚═╝    ▀▄▄▄▄▄▄▀    ░░██░░ ░░███░░\n"
-            "   ██████████████████████████████████████████████████████████████████████████████████████████\n"
-            " ██████████████████████████████████████████████████████████████████████████████████████████████" << endl;
-    cout << " Preferred book genre?" << endl;
-    
     ifstream inFile("Pipe_Goodreads100k.csv");
-
-    /*
-    readFile("Part1-GoodReads_100k_books.csv");
-    readFile("Part2-GoodReads_100k_books.csv");
-    readFile("Part3-GoodReads_100k_books.csv");
-    readFile("Part4-GoodReads_100k_books.csv");
-    readFile("Part5-GoodReads_100k_books.csv");
-    readFile("Part6-GoodReads_100k_books.csv");
-    readFile("Part7-GoodReads_100k_books.csv");
-    readFile("Part8-GoodReads_100k_books.csv");
-    readFile("Part9-GoodReads_100k_books.csv");
-    readFile("Part10-GoodReads_100k_books.csv");
-     */
+    set<string> allGenresFound;
 
     if (inFile.is_open()) {
-        string book, title, author, desc, genre, format, IDstr, pagesStr, ratingStr, temp, genreTemp;
-        int pages, ID;
-        double rating;
-
         string lineFromFile;
         getline(inFile, lineFromFile);
         getline(inFile, lineFromFile);
 
         while (getline(inFile, lineFromFile)) {
             // author|bookformat|desc|genre|img|isbn|isbn13|link|pages|rating|reviews|title|totalratings
+            string book, title, author, desc, genre, format, IDstr, pagesStr, ratingStr, temp, genreTemp;
+            int pages, ID;
+            set<string> genres;
+
             istringstream stream(lineFromFile);
 
             getline(stream, author, '|');
@@ -73,36 +47,57 @@ int main() {
 
             stringstream g(genre);
 
+            //add it into the individual books genre set
             while (getline(g, genreTemp, ',')) {
                 genres.insert(genreTemp);
                 getline(g, genreTemp, ',');
                 genres.insert(genreTemp);
             }
 
-            /*
-            cout << "Author: " << author << endl;
-            cout << "Format: " << format << endl;
-            cout << "Desc: " << desc << endl;
-            cout << "Genre: " << genre << endl;
-            cout << "Pages: " << pagesStr << endl;
-            cout << "Rating: " << ratingStr << endl;
-            cout << "Title: " << title << endl;
-            */
-
-            /*
-            if (!pagesStr.empty()) {
-                pages = stoi(pagesStr);
+            //add into overall list of genres
+            set<string>::iterator itr;
+            for(itr = genres.begin(); itr != genres.end(); itr++){
+                allGenresFound.insert(*itr);
             }
-            if (!ratingStr.empty()) {
-                rating = stoi(ratingStr);
-            }
-            */
 
-            // vector<string> genre, string title, string description,int pages, string author, double rating
-            // * Turn genre into a string, add format
-            tree.insert(genre, title, desc, pages, author, rating, format);
+            bool use = true;
+            if(!pagesStr.empty()){
+                for(int i = 0; i < pagesStr.size(); i++){
+                    if(!isdigit(pagesStr[0])){
+                        use = false;
+                    }
+                }
+
+                if(use && (stol(pagesStr) > 2000 || stol(pagesStr) == 0)){
+                    use = false;
+                }
+
+                if(use){
+                    pages = stoi(pagesStr);
+                    tree.insert(genres, title, desc, pages, author, ratingStr, format);
+
+                }
+            }
 
         }
     }
+
+    cout << endl;
+    cout << "       █░█░█  █▀▀  █    █▀▀  █▀█  █▀▄▀█  █▀▀   ▀█▀  █▀█\n"
+            "       ▀▄▀▄▀  ██▄  █▄▄  █▄▄  █▄█  █░▀░█  ██▄   ░█░  █▄█\n"
+            "                                                                                     ██\n"
+            "     ██████╗░███████╗░█████╗░██████╗░░░░░░░░█████╗░██████╗░                ░░░░░░  ██  ██\n"
+            "     ██╔══██╗██╔════╝██╔══██╗██╔══██╗░░░░░░██╔══██╗██╔══██╗    ▄▀▀▀▀▀▀▄    ░░██░░    ██\n"
+            "     ██████╔╝█████╗░░███████║██║░░██║█████╗███████║██████╔╝   █╗░░▐░░░╔█   ░░██░░ ░░░░░░░\n"
+            "     ██╔══██╗██╔══╝░░██╔══██║██║░░██║╚════╝██╔══██║██╔══██╗   █║░░▐▄▄░║█   ░░██░░ ░░███░░\n"
+            "     ██║░░██║███████╗██║░░██║██████╔╝░░░░░░██║░░██║██║░░██║   █╝░░░░░░╚█   ░░██░░ ░░███░░\n"
+            "     ╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═════╝░░░░░░░╚═╝░░╚═╝╚═╝░░╚═╝    ▀▄▄▄▄▄▄▀    ░░██░░ ░░███░░\n"
+            "   ██████████████████████████████████████████████████████████████████████████████████████████\n"
+            " ██████████████████████████████████████████████████████████████████████████████████████████████" << endl;
+
+
+    cout << "Preferred book genre?" << endl;
+    cout << "Preferred book length?" <<endl;
+    cout << "Desired format?" << endl;
 
 }
